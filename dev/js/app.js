b824ce1d0d75f5c7c26acd9fef22c2d2cd3564ed
Jose Flores Vacio <josefvacio@gmail.com>
2021-07-19 13:10:29 -0700

    Dandole funcionalidad al share button que despliega y cierra el menude share dependiendo el caso

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,17 @@
-const shareButton = document.querySelector('share--button');
+const shareButton = document.querySelector('.button--share');
+const shareMenu = document.querySelector('.share__menu--close')
 
-shareButton.addEventListener((e) => {
-    console.log('Hola');
-})
+console.log(shareButton);
+console.log(shareMenu);
+
+const changeStateMenu = () => {
+    if (shareMenu.classList.contains('share__menu--close')){
+        shareMenu.classList.replace('share__menu--close', 'share__menu--open');
+    } else if (shareMenu.classList.contains('share__menu--open')) {
+        shareMenu.classList.replace('share__menu--open', 'share__menu--close');
+    }
+}
+
+shareButton.addEventListener('click', (e) => {
+    changeStateMenu();
+});
